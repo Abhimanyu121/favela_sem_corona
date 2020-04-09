5030d70caa318e6965000c8dc288a9fde695ce7f
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-04-09 18:48:02 -0300

    Scroll view on menu

--- lib/screens/menu_icons_screen.dart
+++ lib/screens/menu_icons_screen.dart
@@ -57,36 +57,38 @@ class MenuIconsScreen extends StatelessWidget {
               ),
               Expanded(
                 flex: 4,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                      child: Image.asset(
-                        'images/logo.png',
-                        width: 200,
-                        height: 150,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                        child: Image.asset(
+                          'images/logo.png',
+                          width: 200,
+                          height: 150,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    line(),
-                    MenuItem(Icons.dashboard, "PAINEL RIO COVID19",
-                            () => {Navigator.pushNamed(context, 'painelcovidrio_screen')}),
-                    MenuItem(Icons.local_hospital, "HOSPITAIS E UPAS", () => {Navigator.pushNamed(context, 'maps_demo')}),
-                    MenuItem(Icons.flash_on, "PRINCIPAIS DÚVIDAS", () => {Navigator.pushNamed(context, 'min_saude_screen')}),
-                    MenuItem(Icons.filter_hdr, "ASSITÊNCIA FUNERÁRIA", () => {Navigator.pushNamed(context, 'iml_screen')}),
-                    line(),
-                    MenuItem(Icons.directions_run, "SUA COMUNIDADE", () => {Navigator.pushNamed(context, 'listview_screen')}),
-                    MenuItem(Icons.contacts, "LIVES & EVENTOS", () => {}),
-                    MenuItem(Icons.pan_tool, "STOP COVID19", () => {Navigator.pushNamed(context, 'quiz_screen')}),
-                    MenuItem(Icons.touch_app, "NOSSAS MÍDIAS SOCIAIS", () => {Navigator.pushNamed(context, 'instagram_screen')}),
-                    line(),
-                    MenuItem(
-                        Icons.chat_bubble_outline, "APOIO PSICOLÓGICO", () => {}),
-                    MenuItem(Icons.record_voice_over, "DISK DENÚNCIA", () => {}),
-                    line(),
-                    MenuItem(Icons.exit_to_app, "LOGOUT", () => {Navigator.pop(context)})
-                  ],
+                      SizedBox(height: 20),
+                      line(),
+                      MenuItem(Icons.dashboard, "PAINEL RIO COVID19",
+                              () => {Navigator.pushNamed(context, 'painelcovidrio_screen')}),
+                      MenuItem(Icons.local_hospital, "HOSPITAIS E UPAS", () => {Navigator.pushNamed(context, 'maps_demo')}),
+                      MenuItem(Icons.flash_on, "PRINCIPAIS DÚVIDAS", () => {Navigator.pushNamed(context, 'min_saude_screen')}),
+                      MenuItem(Icons.filter_hdr, "ASSITÊNCIA FUNERÁRIA", () => {Navigator.pushNamed(context, 'iml_screen')}),
+                      line(),
+                      MenuItem(Icons.directions_run, "SUA COMUNIDADE", () => {Navigator.pushNamed(context, 'listview_screen')}),
+                      MenuItem(Icons.contacts, "LIVES & EVENTOS", () => {}),
+                      MenuItem(Icons.pan_tool, "STOP COVID19", () => {Navigator.pushNamed(context, 'quiz_screen')}),
+                      MenuItem(Icons.touch_app, "NOSSAS MÍDIAS SOCIAIS", () => {Navigator.pushNamed(context, 'instagram_screen')}),
+                      line(),
+                      MenuItem(
+                          Icons.chat_bubble_outline, "APOIO PSICOLÓGICO", () => {}),
+                      MenuItem(Icons.record_voice_over, "DISK DENÚNCIA", () => {}),
+                      line(),
+                      MenuItem(Icons.exit_to_app, "LOGOUT", () => {Navigator.pop(context)})
+                    ],
+                  ),
                 ),
               ),
             ],
