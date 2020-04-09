49569a84d9c9c4b2cb9953a551daba7bec7f8492
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2020-04-09 09:12:28 -0300

    new way to register plugins using GeneratedPluginRegistry

--- android/app/src/main/kotlin/com/itsencrypted/favelasemcorona/MainActivity.kt
+++ android/app/src/main/kotlin/com/itsencrypted/favelasemcorona/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.itsencrypted.favelasemcorona
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
 
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
