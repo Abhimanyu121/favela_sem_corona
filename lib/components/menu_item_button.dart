f1b1fadcb0f95c5df8240443a0ba87af0b12f9f7
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-04-25 02:11:12 -0300

    Changed this component to avoid overflow on the left and right side

--- lib/components/menu_item_button.dart
+++ lib/components/menu_item_button.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 
 class MenuItem extends StatelessWidget {
-MenuItem({this.icon, this.iconText, @required this.onPressed});
+  MenuItem({this.icon, this.iconText, @required this.onPressed});
 
-final IconData icon;
-final String iconText;
-final Function onPressed;
+  final IconData icon;
+  final String iconText;
+  final Function onPressed;
 
-@override
-Widget build(BuildContext context) {
-  return FlatButton(
-    onPressed: onPressed,
-    child: Row(
-      children: <Widget>[
-        SizedBox(width: 40),
-        Icon(icon, color: Colors.grey),
-        SizedBox(width: 20),
-        Text(iconText, style: TextStyle(fontFamily: 'MontserratAlternates',
-            fontWeight:
-        FontWeight.bold, color: Colors.grey.shade600, fontSize: 16))
-      ],
-    ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 250,
+      child: ListTile(
+        onTap: onPressed,
+        title: Text(iconText),
+        leading: Icon(icon, color: Colors.grey),
+      ),
+    );
+  }
 }
