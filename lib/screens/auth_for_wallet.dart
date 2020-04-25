f92224916794615600e35524f171e548ec5fae3f
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-04-25 16:26:48 +0530

    Added Auth

--- lib/screens/auth_for_wallet.dart
+++ lib/screens/auth_for_wallet.dart
@@ -1,12 +1,20 @@
 import 'package:favelasemcorona/constants.dart';
+import 'package:favelasemcorona/screens/wallet.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth extends StatefulWidget{
+  static const id ="auth";
   @override
   AuthState createState() => AuthState();
 }
 
 class AuthState extends State<Auth>{
+  bool otpUi=false;
+  TextEditingController otp = TextEditingController();
   TextEditingController mobileNumber = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -20,60 +28,40 @@ class AuthState extends State<Auth>{
         title: Text("Create New Wallet", style: TextStyle(color: Colors.black),),
         iconTheme: IconThemeData(color: Colors.black),
       ),
-    //  body: ,
-    );
-  }
-  _loginWithPhone(){
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Hero(
-            child: Container(child: Image.network('https://raw.githubusercontent.com/itsencrypted/favela_sem_corona_api/master/images/logo.png'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(child: Image.network('https://raw.githubusercontent.com/itsencrypted/favela_sem_corona_api/master/images/logo.png'),
               height:MediaQuery.of(context).size.height*0.35,),
-            tag: 'logo-favelasemcorona',
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            autovalidate: false,
-            validator: (val) => (val.isEmpty||val.length==10)
-                ? null
-                : 'Invalid Phone number.',
-            decoration: InputDecoration(
-              hintText: 'Enter Phone Number',
-              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(3.0)),
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.05 ,
             ),
-            controller: mobileNumber,
-          ),
-        ],
-      ),
+            RaisedButton(
+              onPressed: ()async{
+                final GoogleSignIn _googleSignIn = GoogleSignIn();
+                final FirebaseAuth _auth = FirebaseAuth.instance;
+                final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+                final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+                final AuthCredential credential = GoogleAuthProvider.getCredential(
+                  accessToken: googleAuth.accessToken,
+                  idToken: googleAuth.idToken,
+                );
+                final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                print(user.email);
+                prefs.setString(prefEmail,user.email);
+                prefs.setString(prefUser, user.displayName);
+                Navigator.pop(context);
+              },
+              color: Colors.red,
+              child: Text("Login with Google",style: TextStyle(color: Colors.white),),
+            )
+          ],
+        ),
+      )
     );
   }
-  _enterOTP(){
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Hero(
-            child: Container(child: Image.network('https://raw.githubusercontent.com/itsencrypted/favela_sem_corona_api/master/images/logo.png'),
-              height:MediaQuery.of(context).size.height*0.35,),
-            tag: 'logo-favelasemcorona',
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            autovalidate: false,
-            validator: (val) => (val.isEmpty||val.length==4)
-                ? null
-                : 'Invalid OTP.',
-            decoration: InputDecoration(
-              hintText: 'Enter OTP',
-              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(3.0)),
-            ),
-            controller: mobileNumber,
-          ),
-          //RaisedButton
-        ],
-      ),
-    );
-  }
+
 }
